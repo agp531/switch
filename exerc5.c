2547ceee5dcc59a5337386e57d51ed87fd397845
Allan Gabriel <107281173+agp531@users.noreply.github.com>
2022-09-06 17:25:08 -0300

    Add files via upload

--- exerc5.c
+++ exerc5.c
@@ -1,68 +1,72 @@
 #include <stdio.h>
-#include <math.h>
-
-	int main (){
-		
-		int compra,menu,desc,parc;
 	
+	int main(){
+		
+		int i,j,matrizA[10][10];
+		char menu;
 			
-			printf("Informe o valor total da compra R$: ");
-			scanf(" %d",&compra);
-			printf(" =====================================================================\n");
-			printf(" |                Escolha a forma de pagamento:                      |\n");
-			printf(" | [1] Pagamento a vista - 15%% de desconto                           |\n");
-			printf(" | [2] Pagamento com cheque pre-datado para 30 dias - 10%% de desconto|\n");	
-			printf(" | [3] Pagamento parcelado em 3 vezes - 5%% de desconto.              |\n");
-			printf(" | [4] Pagamento parcelado em 6 vezes -sem desconto.                 |\n");
-			printf(" | [5] Pagamento parcelado em 12 vezes - 8%% de juros.                |\n");
-			printf(" =====================================================================\n");
+			for(i=0;i<2;i++){
+				for(j=0;j<2;j++){
+					printf("Informe o valor da matriz[%d][%d]\n",i+1,j+1);
+					scanf("%d",&matrizA[i][j]);
+				}
+			}
 			
-			printf("  Digite a opcao desejada: ");
-			scanf("  %d",&menu);
+			printf("\t ===========================================================================================\n");
+			printf("\t |                       Escolha o que deseja exibir a seguir:                             |\n");
+			printf("\t |                                                                                         |\n");
+			printf("\t | [A] Mostrar todos os elementos da matriz (na forma de uma matriz LxC).                  |\n");
+			printf("\t | [B] Mostrar todos os elementos da quarta linha da matriz e da primeira coluna da matriz.|\n");	
+			printf("\t | [C] Mostrar a soma dos elementos da diagonal principal.                                 |\n");
+			printf("\t | [D] Atribuir o valor 0 para os valores negativos encontrados fora da diagonal principal.|\n");
+			printf("\t | [E] Buscar um elemento na matriz.                                                       |\n");
+			printf("\t |                                                                                         |\n");
+			printf("\t ===========================================================================================\n");
+			
+			printf("\n Digite a opcao desejada: ");
+			scanf(" %c",&menu);
 			printf("\n");
 			
 			switch(menu){
-				
-				case 1:
-					desc = compra *85/100;
-					printf(" Valor total R$:%d \n",compra);
-					printf(" Valor avista com 15%% de desconto R$:%d ,00",desc);
-					break;
-				case 2:
-					desc = compra *90/100;
-					printf(" Valor total R$:%d \n",compra);
-					printf(" Valor final com 10%% de desconto R$:%d",desc);
-					break;	
-				case 3:
-					desc = compra *95/100;
-					parc = desc / 3;
-					printf(" Valor total R$:%d,00 \n",compra);
-					printf(" Valor final com 5%% de desconto R$:%d,00 \n",desc);
-					printf(" Valor das parcelas 3x R$:%d,00",parc);
-					break;
-					
-				case 4:
-					parc = compra / 6 ;
-					printf(" Valor total R$:%d,00 \n",compra);
-					printf(" Opcao selecionada nao tem desconto \n");
-					printf(" Valor das parcelas 6x R$:%d,00",parc);
-					break;
-					
-				case 5:
-					desc = compra * 108/100;
-					parc = desc / 12;
-					printf(" Valor total R$:%d,00 \n",compra);
-					printf(" Valor final com juros de 8%% R$:%d \n",desc);
-					printf(" Valor das parcelas 12x R$:%d,00",parc);
-				
-							
-				default:
-				printf(" Opcao selecionada invalida.");
-				
-			}	
 			
+				case 'A':
+				case 'a':
+				
+					for(i=0;i<2;i++){
+						for(j=0;j<2;j++){
+							printf("%d \t",matrizA[i][j]);
+						}
+						printf("\n");
+					}
+					
+					break;
+					
+				case 'B':
+				case 'b':
+					printf("funcionando aqui");
+					break;
+					
+				case 'C':
+				case 'c':
+				
+					break;
+				
+				case 'D':
+				case 'd':
+				
+					break;
+					
+				case 'E':
+				case 'e':
+				
+					break;
+			}
+				
 		
 		
 		
-		return 0; 
+		return 0;
 	}
+
+
+
